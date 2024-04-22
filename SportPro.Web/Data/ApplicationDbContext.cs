2c32b44b8134cc6a98c43091b91c9596b2f707f4
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-04-22 14:52:52 +0200

    Created DbContext

---
+++ SportPro.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportPro.Web.Models.Domains;
+
+namespace SportPro.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Natjecaji> Natjecaji { get; set; }
+
+}
