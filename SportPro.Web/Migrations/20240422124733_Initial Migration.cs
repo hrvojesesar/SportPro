0e1e5dd7abf90339facda4e06ff6e47f9f75af32
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-04-22 14:53:24 +0200

    Created first migrations for natjecaji

---
+++ SportPro.Web/Migrations/20240422124733_Initial Migration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportPro.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Natjecaji",
+                columns: table => new
+                {
+                    IDNatjecaj = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    Opis = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProcijenjenaVrijednost = table.Column<int>(type: "int", nullable: false),
+                    TrajanjeOd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TrajanjeDo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DatumObjave = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Aktivan = table.Column<bool>(type: "bit", nullable: false),
+                    Dobitnik = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Natjecaji", x => x.IDNatjecaj);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Natjecaji");
+        }
+    }
+}
