4a01a40443e95cc03123dda3a052888282b6266d
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-05-20 16:09:53 +0200

    Added validations for Certifikati

--- SportPro.Web/Models/ViewModels/AddCertifikatRequest.cs
+++ SportPro.Web/Models/ViewModels/AddCertifikatRequest.cs
@@ -6,9 +6,13 @@ public class AddCertifikatRequest
 {
     [Key]
     public int IDCertifikat { get; set; }
+    [Required]
+    [StringLength(100, ErrorMessage = "Naziv ne može imati više od 100 znakova!")]
     public string Naziv { get; set; }
     public string? Opis { get; set; }
+    [Required]
     public DateTime DatumDodjele { get; set; }
+    [Required]
     public string Organizacija { get; set; }
     public string Slika { get; set; }
 }
